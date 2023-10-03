5dcdb85a74dc52ea2cc22e03ef5e87d655774e1d
likedidu <91383267+likedidu@users.noreply.github.com>
2023-10-03 17:11:11 +0800

    mod

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ ENV PM2_HOME=/tmp
 RUN set -ex \
     && yarn install \
     && yarn global add pm2 \
-    && chmod +x sing-box entrypoint.sh \
+    && chmod +x entrypoint.sh \
     && addgroup --gid 10014 choreo \
     && adduser --disabled-password  --no-create-home --uid 10014 --ingroup choreo choreouser \
     && usermod -aG sudo choreouser
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,15 @@
 set -e
 exec 2>&1
 
-# 设置各变量
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
 WSPATH=${WSPATH:-'argo'}
+
+# install sing-box
+DIR_TMP="$(mktemp -d)"
+EXEC=$(echo $RANDOM | md5sum | head -c 4)
+wget -O - 'https://github.com/SagerNet/sing-box/releases/download/v1.5.0/sing-box-1.5.0-linux-amd64.tar.gz' | tar xz -C ${DIR_TMP}
+install -m 755 ${DIR_TMP}/sing-box*/sing-box /usr/src/app/app${EXEC}
+rm -rf ${DIR_TMP}
 
 generate_config() {
   cat > /tmp/config.json << EOF
@@ -96,7 +102,7 @@ module.exports = {
   apps: [
     {
       name: "web",
-      script: "/usr/src/app/sing-box run -c /tmp/config.json"
+      script: "/usr/src/app/app* run -c /tmp/config.json"
     }
   ]
 }
